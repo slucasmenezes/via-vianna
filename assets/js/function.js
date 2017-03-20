606e0c838f365a08b5930be962f9f38a1de54e9d
Lucas Menezes <slucasmenezes@gmail.com>
2017-03-20 17:35:07 -0300

    Corigindo a validacao do formulario de contato

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,4 +1,32 @@
 $(document).ready(function(){
   
+  $("form").submit(function(){
+    var name = $(':input[name=Nome]').val();
+    var email = $(':input[name=Email]').val();
+    var mensage = $(':input[name=Mensagem]').val();
+
+    var error = "";
+    $("#errors").html("");
+
+    if (name ==="") {
+      error += "<p>Digite um nome</p>"
+    }
+
+    if (email ===""){
+      error += "<p>Digite um e-mail</p>"
+    }
+
+    if (mensage ==="") {
+      error += "<p>Digite uma Mensagem</p>"
+    }
+
+    if (error) {
+      $("#errors").html(error)
+      return false;
+    }
+
+    return true;
+  });
+
 });
 
