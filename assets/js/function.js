04cf1a95a5b3ca7ddf7e79cd4dd56db9d4144403
Lucas Menezes <slucasmenezes@gmail.com>
2017-03-20 11:07:34 -0300

    Criando o arquivo de script separado

---
+++ assets/js/function.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  
+  function hgsubmit() {
+    if (/\S+/.test(document.hgmailer.nome.value) == false) alert ("Por favor, digite um nome.");
+    else if (/^\S+@[a-z0-9_.-]+\.[a-z]{2,6}$/i.test(document.hgmailer.email.value) == false) alert ("Um endereço de e-mail válido é requerido.");
+    else if (/\S+/.test(document.hgmailer.mensagem.value) == false) alert ("É necessário um conteúdo para mensagem.");
+    else { 
+      document.hgmailer.submit();
+      alert ('Obrigado!\nSeu e-mail foi enviado com sucesso.');
+    }
+  }  
+  
+});
+
